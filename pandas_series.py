f6f27f86f6a23f0b4654690da4e2509f404acf68
masonsherbondy <sherbondy.mason@gmail.com>
2021-10-14 15:11:05 -0500

    panda panda

--- pandas_series.py
+++ pandas_series.py
@@ -17,35 +17,38 @@ fruits = pd.Series(["kiwi", "mango", "strawberry", "pineapple", "gala apple", "h
 # In[ ]:
 
 
-#Exercise 1
+#Exercise 1 Determine the number of elements in fruits.
 fruits.size
 
 
 # In[ ]:
 
 
-#Exercise 2
+#Exercise 2 Output only the index from fruits.
 fruits.index
 
 
 # In[ ]:
 
 
-#Exercise 3
+#Exercise 3 Output only the values from fruits.
 fruits.values
 
 
 # In[ ]:
 
 
-#Exercise 4
+#Exercise 4 Confirm the data type of the values in fruits.
 fruits.dtype
 
 
 # In[ ]:
 
 
-#Exercise 5
+#Exercise 5 
+#Output only the first five values from fruits. 
+#Output the last three values. 
+#Output two random values from fruits.
 fruits.head()
 fruits.tail(3)
 fruits.sample(2)
@@ -54,28 +57,29 @@ fruits.sample(2)
 # In[ ]:
 
 
-#Exercise 6
+#Exercise 6 
+#Run the .describe() on fruits to see what information it returns when called on a Series with string values.
 fruits.describe()
 
 
 # In[ ]:
 
 
-#Exercise 7
+#Exercise 7 Run the code necessary to produce only the unique string values from fruits.
 fruits.unique()
 
 
 # In[ ]:
 
 
-#Exercise 8
+#Exercise 8 Determine how many times each unique string value occurs in fruits.
 fruits.nunique()
 
 
 # In[ ]:
 
 
-#Exercise 9
+#Exercise 9 Determine the string value that occurs most frequently in fruits.
 fruits.value_counts().idxmax()
 
 #all max ids
@@ -85,7 +89,7 @@ fruits.value_counts().nlargest(n = 1, keep = 'all')
 # In[ ]:
 
 
-#Exercise 10
+#Exercise 10 Determine the string value that occurs least frequently in fruits.
 #all min ids
 fruits.value_counts().nsmallest(n = 1, keep = 'all')
 
