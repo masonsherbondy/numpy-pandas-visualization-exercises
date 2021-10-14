3e6d963bb1296dc17a8dc8ebe94289ad850794b6
masonsherbondy <sherbondy.mason@gmail.com>
2021-10-14 15:30:05 -0500

    huh

--- pandas_series.py
+++ pandas_series.py
@@ -6,7 +6,10 @@
 
 import numpy as np
 import pandas as pd
+import mason_functions as mf
 
+
+# ## Exercises Part I
 
 # In[ ]:
 
@@ -92,4 +95,70 @@ fruits.value_counts().nlargest(n = 1, keep = 'all')
 #Exercise 10 Determine the string value that occurs least frequently in fruits.
 #all min ids
 fruits.value_counts().nsmallest(n = 1, keep = 'all')
+
+
+# # Exercises Part II
+
+# In[ ]:
+
+
+#Exercise I Capitalize all the string values in fruits.
+fruits.str.capitalize()
+
+
+# In[ ]:
+
+
+#Exercise II Count the letter "a" in all the string values (use string vectorization).
+fruits.str.count('a')
+
+
+# In[ ]:
+
+
+#Exercise III Output the number of vowels in each and every string value.
+fruits.apply(mf.count_vowels)
+
+
+# In[ ]:
+
+
+#Exercise IV Write the code to get the longest string value from fruits.
+max(fruits, key = len)
+
+
+# In[ ]:
+
+
+#Exercise V Write the code to get the string values with 5 or more letters in the name.
+fruits.apply(lambda fruit: len(fruit) > 4)
+
+
+# In[ ]:
+
+
+#Exercise VI 
+#Use the .apply method with a lambda function to find the fruit(s) containing the letter "o" two or more times.
+fruits.apply(lambda fruit: fruit.count('o') > 1)
+
+
+# In[ ]:
+
+
+#Exercise VII Write the code to get only the string values containing the substring "berry".
+fruits.str.endswith('berry')
+
+
+# In[ ]:
+
+
+#Exercise VIII Write the code to get only the string values containing the substring "apple".
+fruits.str.endswith('apple')
+
+
+# In[ ]:
+
+
+#Exercise IX Which string value contains the most vowels?
+max(fruits, key = mf.count_vowels)
 
